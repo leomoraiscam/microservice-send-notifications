36bb33c022dc168915f99fd924890868f6579b61
lmorais <lmorais.contato@gmail.com>
2023-02-13 16:54:42 -0300

    feat: create send notification use case

---
+++ src/app/usecases/send-notification.ts
@@ -0,0 +1,33 @@
+import { Notification } from '../entities/notifications';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+interface SendNotificationRequest {
+  recipientId: string;
+  content: string;
+  category: string;
+}
+
+interface SendNotificationResponse {
+  notification: Notification;
+}
+
+export class SendNotification {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: SendNotificationRequest,
+  ): Promise<SendNotificationResponse> {
+    const { recipientId, category, content } = request;
+
+    const notification = new Notification({
+      recipientId,
+      content,
+      category,
+    });
+
+    await this.notificationRepository.create(notification);
+
+    return {
+      notification,
+    };
+  }
+}
