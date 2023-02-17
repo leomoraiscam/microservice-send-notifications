6238aa9af47fca627c220118e125a902a4efb5d8
lmorais <lmorais.contato@gmail.com>
2023-02-16 21:54:38 -0300

    feat: create mapper layer to repository and controller

---
+++ src/infra/http/view-models-mapper/notification-view-model-mapper.ts
@@ -0,0 +1,12 @@
+import { Notification } from '@application/entities/notifications';
+
+export class NotificationViewModelMapper {
+  static toHTTP(notification: Notification) {
+    return {
+      id: notification.id,
+      content: notification.content,
+      category: notification.category,
+      recipientId: notification.recipientId,
+    };
+  }
+}
