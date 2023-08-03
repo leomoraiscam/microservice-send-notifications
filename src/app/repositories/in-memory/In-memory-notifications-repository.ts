5182a6ec91361435bdce90b9e7071323e9f04bc7
lmorais <lmorais.contato@gmail.com>
2023-08-03 09:40:00 -0300

    test: implementation in memory notification repository to use tests case

---
+++ src/app/repositories/in-memory/In-memory-notifications-repository.ts
@@ -0,0 +1,12 @@
+import { NotificationsRepository } from '../notifications-repository';
+import { Notification } from '../../entities/notification';
+
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository
+{
+  public notifications: Notification[] = [];
+
+  async create(notification: Notification): Promise<void> {
+    this.notifications.push(notification);
+  }
+}
