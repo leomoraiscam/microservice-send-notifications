f22d5ebaf532f1cbee42193a54faa74b9103463f
lmorais <lmorais.contato@gmail.com>
2023-08-03 20:23:15 -0300

    test: create unread notifications use case

---
+++ src/app/usecases/unread-notification.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryNotificationsRepository } from '../../../tests/repositories/in-memory-notifications-repository';
+import { UnreadNotification } from './unread-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { makeNotification } from '../../../tests/factories/notification-factory';
+
+describe('Unread notification', () => {
+  it('should be able to unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  it('should not be unread to read a notification when it does not exist', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    await expect(
+      unreadNotification.execute({
+        notificationId: 'fake-notification-id',
+      }),
+    ).rejects.toBeInstanceOf(NotificationNotFound);
+  });
+});
