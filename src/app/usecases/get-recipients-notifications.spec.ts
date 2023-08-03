9d6b95901c30409acb86d1329889015462ebabe5
lmorais <lmorais.contato@gmail.com>
2023-08-03 20:17:25 -0300

    test: create get notifications use case by recipient

---
+++ src/app/usecases/get-recipients-notifications.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryNotificationsRepository } from '@app/repositories/in-memory/In-memory-notifications-repository';
+import { GetRecipientNotifications } from './get-recipients-notifications';
+import { makeNotification } from '../../../tests/factories/notification-factory';
+
+describe('Get recipient notifications', () => {
+  it('should be able to get recipient notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'recipient-1',
+      }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({
+        recipientId: 'recipient-2',
+      }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(notifications.length).toEqual(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
+  });
+});
