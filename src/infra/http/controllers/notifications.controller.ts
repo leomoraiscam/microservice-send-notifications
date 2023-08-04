ba07fa590b57da24deb48b3d8e7c4195872f9131
lmorais <lmorais.contato@gmail.com>
2023-08-03 22:13:37 -0300

    feat: exposed route to receive acess to countFromRecipient

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { SendNotification } from '@app/usecases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { NotificationViewModelMapper } from '../view-models-mapper/notification-view-model-mapper';
+import { CountRecipientNotification } from '@app/usecases/count-recipient-notifications';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private countRecipientNotifications: CountRecipientNotification,
+  ) {}
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.countRecipientNotifications.execute({
+      recipientId,
+    });
+
+    return {
+      count,
+    };
+  }
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
