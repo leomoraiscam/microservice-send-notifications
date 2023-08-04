709e9e4d6beb2f75d84b49cdc7ead0ea09e35ea5
lmorais <lmorais.contato@gmail.com>
2023-08-03 22:20:37 -0300

    feat: add services in http module provider

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
 import { SendNotification } from '@app/usecases/send-notification';
 import { DatabaseModule } from '../database/database.module';
+import { GetRecipientNotifications } from '@app/usecases/get-recipients-notifications';
+import { CountRecipientNotification } from '@app/usecases/count-recipient-notifications';
+import { UnreadNotification } from '@app/usecases/unread-notification';
+import { ReadNotification } from '@app/usecases/read-notification';
+import { CancelNotification } from '@app/usecases/cancel-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    GetRecipientNotifications,
+    CountRecipientNotification,
+    UnreadNotification,
+    ReadNotification,
+    CancelNotification,
+  ],
 })
 export class HttpModule {}
